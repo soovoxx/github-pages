bbcb291d5c8ac0a91055c3d57ba90321b5471361
Jungsuu <swift779@naver.com>
2025-12-08 15:55:37 +0900

    remove docs folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://soovoxx.github.io",
   base: "/github-pages", // 레포 이름
-  outDir: "./docs", // 빌드 결과를 docs 폴더에
 });
