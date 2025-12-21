fe09fd666dc310906774e66d53f0e7a512dc9ad4
soovoxx <swift779@naver.com>
2025-12-21 20:21:16 +0900

    chore: astro mdx 확장 추가 및 적용

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://soovoxx.github.io",
   base: "/github-pages", // 레포 이름
+  integrations: [mdx()],
 });
