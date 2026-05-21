3f080a3ea717312c80b7054fefcd0b3e63c93e7f
soovoxx <swift779@naver.com>
2026-05-21 22:54:04 +0900

    fix: astro version 변경으로 인한 설정 변경

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,12 @@
-import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 
 const posts = defineCollection({
+  loader: glob({
+    base: "./src/content/posts",
+    pattern: "**/*.{md,mdx}",
+  }),
   schema: z.object({
     title: z.string(),
     pubDate: z.string(),
